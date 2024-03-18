f8dd0c17c8e0e1f472e6b9d60a8a3a82fdeff368
elizaveta-vasileva <78210793+Elizaveta-Vasileva@users.noreply.github.com>
2024-03-18 11:37:38 +0100

    Update +page.svelte

--- src/routes/rooms/+page.svelte
+++ src/routes/rooms/+page.svelte
@@ -9,8 +9,8 @@
                     <img id='room1_img' src="images/room_1.jpg" alt="Room 1" class="w-full h-64 object-cover">
                 </div>
                 <div class="p-6">
-                    <h3 class="text-xl font-semibold mb-2"> Room 1</h3>
-                    <p class="text-gray-600">Description.</p>
+                    <h3 class="text-xl font-semibold mb-2"> Superior Room</h3>
+                    <p class="text-gray-600">Unwind in this contemporary room with one double bed and a work desk with USB ports for your business needs. The bathroom boasts a heated floor and a spacious walk-in rain shower. Relax and watch satellite channels on your HDTV or connect with friends and family using free WiFi. Other comforts include air-conditioning, a safe and a mini refrigerator.</p>
                     <a href="book" class="bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-full inline-block mt-4">Book Now</a>
                 </div>
             </div> 
@@ -20,8 +20,8 @@
                     <img id= 'room2_img' src="images/room_2.jpg" alt="Room 2" class="w-full h-64 object-cover">
                 </div>
                 <div class="p-6">
-                    <h3 class="text-xl font-semibold mb-2"> Room 2</h3>
-                    <p class="text-gray-600">Description.</p>
+                    <h3 class="text-xl font-semibold mb-2">Deluxe Room</h3>
+                    <p class="text-gray-600">Unwind in this spacious, modern room with one queen bed. The bathroom features a heated floor and a spacious walk-in shower. Amenities include HDTV, USB ports, complimentary WiFi, air conditioning, a safe and a mini refrigerator.</p>
                     <a href="book" class="bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-full inline-block mt-4">Book Now</a>
                 </div>
             </div> 
@@ -31,8 +31,8 @@
                     <img id='room3_img' src="images/room_3.jpg" alt="Room 3" class="w-full h-64 object-cover">
                 </div>
                 <div class="p-6">
-                    <h3 class="text-xl font-semibold mb-2"> Room 3</h3>
-                    <p class="text-gray-600">Description.</p>
+                    <h3 class="text-xl font-semibold mb-2">Family Connecting Room</h3>
+                    <p class="text-gray-600">Enjoy a family break in Paris in our comfortable and convenient family option of two interconnecting rooms, each with a queen bed. Each room offers complimentary WiFi and an HDTV with children's channels. Other features include a Nespresso machine, bathrobes and slippers. Special touches include a VIP welcome gift for families.</p>
                     <a href="book" class="bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-full inline-block mt-4">Book Now</a>
                 </div>
             </div> 
@@ -42,8 +42,8 @@
                     <img id='room4_img' src="images/room_4.jpg" alt="Room 4" class="w-full h-64 object-cover">
                 </div>
                 <div class="p-6">
-                    <h3 class="text-xl font-semibold mb-2"> Room 4</h3>
-                    <p class="text-gray-600">Description.</p>
+                    <h3 class="text-xl font-semibold mb-2">Executive Room</h3>
+                    <p class="text-gray-600">Unwind in this large, modern room with one large bed or twin beds (on request and upon availability). The bathroom features a heated floor and a spacious walk-in shower. Amenities include HDTV, USB ports, complimentary WiFi, air conditioning, a safe, a mini refrigerator and a Nespresso machine. Sleeps up to 3 people (third person on a rollaway bed, extra charge may apply).</p>
                     <a href="book" class="bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-full inline-block mt-4">Book Now</a>
                 </div>
             </div> 
