e98e8e9ed5fefadc47f79dab53a99c891fa603fd
elizaveta-vasileva <78210793+Elizaveta-Vasileva@users.noreply.github.com>
2024-03-23 19:53:15 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=0.8">
-    <!-- Include Tailwind CSS via link tag -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
 </head>
 
